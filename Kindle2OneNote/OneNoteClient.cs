1a7bf873a48f006b9ac0fceaa95c1ec7ee04baff
ld0891 <ld0891@live.com>
2017-02-18 18:50:14 +0800

    add client class for OneNote

---
+++ Kindle2OneNote/OneNoteClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kindle2OneNote
+{
+    public sealed class OneNoteClient
+    {
+        private static volatile OneNoteClient instance = null;
+        private static object syncRoot = new Object();
+
+        private OneNoteClient() { }
+
+        public static OneNoteClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new OneNoteClient();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public List<string> fetchNotebooks()
+        {
+            List<string> notebookNames = new List<string>();
+            notebookNames.Add("readings");
+            notebookNames.Add("writings");
+            notebookNames.Add("belongings");
+            return notebookNames;
+        }
+    }
+}
