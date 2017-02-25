e43cc8b633248882a155c4a14fc6ef0630536a39
ld0891 <ld0891@live.com>
2017-02-25 17:35:59 +0800

    search removable disks for clipping file

---
+++ Kindle2OneNote/Kindle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+
+namespace Kindle2OneNote
+{
+    public sealed class Kindle
+    {
+        private static volatile Kindle instance = null;
+        private static object syncRoot = new Object();
+
+        private static readonly String clippingFilePath = @"documents/My Clippings.txt";
+
+        private Kindle() { }
+
+        public static Kindle Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Kindle();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task<StorageFile> GetClippingFile()
+        {
+            StorageFile file = null;
+            StorageFolder externalDevices = Windows.Storage.KnownFolders.RemovableDevices;
+            IReadOnlyList<StorageFolder> devices = await externalDevices.GetFoldersAsync();
+            foreach (StorageFolder device in devices)
+            {
+                try
+                {
+                    file = await device.GetFileAsync(clippingFilePath);
+                    break;
+                }
+                catch
+                {
+                    file = null;
+                    continue;
+                }
+            }
+            return file;
+        }
+    }
+}
